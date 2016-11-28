705d87f4a3b7a74a411214b20efff508552f3e6b
Renato Lui Geh <renatogeh@gmail.com>
2016-11-28 16:54:22 -0200

    Time usage graph.
    Former-commit-id: cbf0977586acb659c361c7f0532d734111a9a814

--- results/digits/get_delta.c
+++ results/digits/get_delta.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *args[]) {
   double min, max, t;
-  int i, n;
+  int i, n, f;
 
   if (argc < 3) {
-    printf("Usage: %s [p]...\n"
-        "Prints the lowest and highest p values.\n", args[0]);
+    printf("Usage: %s [p]... [-f]\n"
+        "Prints the lowest and highest p values. Floors if -f.\n", args[0]);
     return 1;
   }
 
+  f = !strcmp(args[argc-1], "-f");
   min = max = atof(args[1]);
-  for (i=1, n=argc-1; i < n; ++i) {
+  for (i=1, n=f?argc-2:argc-1; i < n; ++i) {
     t = atof(args[i+1]);
     if (t > max)
       max = t;
@@ -20,7 +22,10 @@ int main(int argc, char *args[]) {
       min = t;
   }
 
-  printf("%.2f %.2f\n", min, max);
+  if (f)
+    printf("%d %d\n", (int) min, (int) max);
+  else
+    printf("%.2f %.2f\n", min, max);
 
   return 0;
 }
--- results/digits/parse.sh
+++ results/digits/parse.sh
@@ -47,23 +47,129 @@ for i in `seq 0 8`; do
   echo " 0.$j ${avg:0:lend} ${min_max[0]} ${min_max[1]}" >> avgs.dat
 done
 
-rm get_delta.out
-
 # Create Gnuplot script.
-:> $1.gpi
-echo "set title \"Dataset $1 for p=[0.1, 0.9]: percentage of correct classifications\"" >> $1.gpi
-echo "set xrange [0:1]" >> $1.gpi
-echo "set xtics 0.1" >> $1.gpi
-echo "set yrange [90:100]" >> $1.gpi
-echo "set format y \"%.0f%%\"" >> $1.gpi
-echo "set ytics 1" >> $1.gpi
-echo "set key outside" >> $1.gpi
-echo "set term png size 800,500" >> $1.gpi
-echo "set output \"$1.png\"" >> $1.gpi
+out=${1}_percs.gpi
+:> $out
+echo "set title \"Dataset $1 for p=[0.1, 0.9]: percentage of correct classifications\"" >> $out
+echo "set xrange [0:1]" >> $out
+echo "set format x \"%.1f\"" >> $out
+echo "set xtics 0.1" >> $out
+echo "set xlabel \"(p)\nPartition for cross-validation\"" >> $out
+echo "set yrange [90:100]" >> $out
+echo "set format y \"%.0f%%\"" >> $out
+echo "set ytics 1" >> $out
+echo "set ylabel \"Correct classifications\n(\%)\"" >> $out
+echo "set grid" >> $out
+echo "set key outside" >> $out
+echo "set term png size 800,500" >> $out
+echo "set output \"${1}_percs.png\"" >> $out
 echo "plot 'avgs.dat' using 1:2 with linespoints lw 3 title \"Averages\", \
   '' using 1:2:3:4 with errorbars lw 3 title \"Min and max\", \
-  '' using 1:2:(sprintf(\"%.2f%%\", \$2)) with labels center offset 0,-1 notitle" >> $1.gpi
+  '' using 1:2:(sprintf(\"%.2f%%\", \$2)) with labels center offset 0,-1 notitle" >> $out
 
 # Run script.
-gnuplot $1.gpi
-feh $1.png
+gnuplot $out
+#feh ${1}_percs.png
+
+# Find user time elapsed in seconds.
+matches=($(grep -o 'User time (seconds): [0-9]\+\(.[0-9]\+\)*' *.txt))
+n=${#matches[@]}
+j=0
+
+for i in `seq 0 $(( n - 1 ))`; do
+  if [[ $(( i % 4 )) == 3 ]]; then
+    secs[j]=${matches[i]}
+    echo "Run $j lasted: ${matches[i]} seconds."
+    let j=j+1
+  fi
+done
+
+gcc convtime.c -o convtime.out
+
+:> time.dat
+echo "# Gnuplot user time data file for 9 iterations of p." >> time.dat
+echo "# p  h  min  sec" >> time.dat
+
+n=${#secs[@]}
+min_max=( `./get_delta.out ${secs[@]} -f` )
+for i in `seq 1 $n`; do
+  j=$(( i - 1 ))
+  cvt=`./convtime.out ${secs[j]}}`
+  echo "0.$i $cvt" >> time.dat
+done
+
+out=${1}_time.gpi
+:> $out
+echo "set title \"Dataset $1 for p=[0.1, 0.9]: user time\"" >> $out
+echo "set xrange [0:1]" >> $out
+echo "set format x \"%.1f\"" >> $out
+echo "set xtics 0.1" >> $out
+echo "set xlabel \"(p)\nPartition for cross-validation\"" >> $out
+echo "set timefmt \"%H %M %S\"" >> $out
+echo "set ydata time" >> $out
+echo "set format y \"%tM:%.0tS\"" >> $out
+echo "set ylabel \"Elapsed time\n(minutes:seconds)\"" >> $out
+echo "set yrange [$(( min_max[0] - 90 )):$(( min_max[1] + 90 ))]" >> $out
+echo "set grid" >> $out
+echo "set key outside" >> $out
+echo "set term png size 800,400" >> $out
+echo "set output \"${1}_time.png\"" >> $out
+echo "plot 'time.dat' using 1:2:3:4 with linespoints lw 3 title \"Total running time\", \
+  '' using 1:2:(sprintf(\"%d:%02.0f\", \$3, \$4)):3 with labels center offset 0,1 font ',14' notitle" >> $out
+
+# Run script.
+gnuplot $out
+#feh ${1}_time.png
+
+rm convtime.out
+
+# Find memory usage.
+matches=($(grep -o 'Maximum resident set size (kbytes): [0-9]\+' *.txt))
+n=${#matches[@]}
+j=0
+for i in `seq 0 $(( n - 1 ))`; do
+  if [[ $(( i % 6 )) == 5 ]]; then
+    mem[j]=${matches[i]}
+    let j=j+1
+  fi
+done
+
+n=${#mem[@]}
+min_max=`./get_delta.out ${mem[@]} -f`
+
+:> mem.dat
+echo "# Gnuplot memory data file for 9 iterations of p." >> mem.dat
+echo "# p  kbytes mbytes" >> mem.dat
+
+for i in `seq 1 9`; do
+  j=$(( i - 1 ))
+  kb=${mem[j]}
+  mb=`bc <<< "scale=2; $kb / 1000.0"`
+  echo "KB: $kb = MB: $mb"
+  echo " 0.$i $kb $mb" >> mem.dat
+done
+
+# Create Gnuplot script.
+out=${1}_mem.gpi
+:> $out
+echo "set title \"Dataset $1 for p=[0.1, 0.9]: memory usage\"" >> $out
+echo "set xrange [0:1]" >> $out
+echo "set format x \"%.1f\"" >> $out
+echo "set xtics 0.1" >> $out
+echo "set xlabel \"(p)\nPartition for cross-validation\"" >> $out
+echo "set yrange [${min_max[0]}:${min_max[1]}]" >> $out
+echo "set format y \"%d kB\"" >> $out
+#echo "set ytics 1" >> $out
+echo "set ylabel \"Correct classifications\n(\%)\"" >> $out
+echo "set grid" >> $out
+echo "set key outside" >> $out
+echo "set term png size 800,500" >> $out
+echo "set output \"${1}_mem.png\"" >> $out
+echo "plot 'avgs.dat' using 1:2 with linespoints lw 3 title \"Averages\", \
+  '' using 1:2:3:4 with errorbars lw 3 title \"Min and max\", \
+  '' using 1:2:(sprintf(\"%.2f%%\", \$2)) with labels center offset 0,-1 notitle" >> $out
+
+# Preview graphs.
+feh *.png
+
+rm get_delta.out
