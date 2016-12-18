77c95e6311ee8e0cda2c2c448181ffcccdbf2a27
Renato Lui Geh <renatogeh@gmail.com>
2016-12-18 16:16:17 -0200

    Refactoring sum and node.

---
+++ sspn/sum.go
@@ -0,0 +1,75 @@
+package sspn
+
+import (
+	"math"
+	"sort"
+)
+
+// Sum represents a sum node in an SPN.
+type Sum struct {
+	Node
+	w []float64
+}
+
+// Value returns the value of this node given an instantiation.
+func (s *Sum) Value(val VarSet) float64 {
+	n := len(s.ch)
+
+	vals := make([]float64, n)
+	for i := 0; i < n; i++ {
+		v, w := s.ch[i].Value(val), math.Log(s.w[i])
+		vals[i] = v + w
+	}
+	sort.Float64s(vals)
+	p, r := vals[n-1], 0.0
+
+	for i := 0; i < n-1; i++ {
+		r += math.Exp(vals[i] - p)
+	}
+
+	r = p + math.Log1p(r)
+
+	return r
+}
+
+// Max returns the MAP value of this node given an evidence.
+func (s *Sum) Max(val VarSet) float64 {
+	max := math.Inf(-1)
+	n := len(s.ch)
+
+	for i := 0; i < n; i++ {
+		cv := math.Log(s.w[i]) + s.ch[i].Max(val)
+		if cv > max {
+			max = cv
+		}
+	}
+
+	return max
+}
+
+// ArgMax returns both the arguments and the value of the MAP state given a certain valuation.
+func (s *Sum) ArgMax(val VarSet) (VarSet, float64) {
+	n, max := len(s.ch), math.Inf(-1)
+	var mch *Node
+
+	for i := 0; i < n; i++ {
+		ch := s.ch[i]
+		// Note to future self: use DP to avoid recomputations.
+		m := math.Log(s.w[i]) + ch.Max(val)
+		if m > max {
+			max, mch = m, ch
+		}
+	}
+
+	return mch.ArgMax(val)
+}
+
+// Type returns the type of this node.
+func (s *Sum) Type() string {
+	return "sum"
+}
+
+// Weights returns weights if sum product. Returns nil otherwise.
+func (s *Sum) Weights() []float64 {
+	return s.w
+}
