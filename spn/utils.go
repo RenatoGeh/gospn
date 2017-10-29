8fcdab70f50116a64a43bdc1dffe470f32e32c8e
Renato Lui Geh <renatogeh@gmail.com>
2017-10-29 19:56:00 -0200

    StoreMAP and tests.

--- spn/utils.go
+++ spn/utils.go
@@ -76,64 +76,98 @@ func StoreInference(S SPN, I VarSet, tk int, storage *Storer) (SPN, int) {
 // StoreMAP takes an SPN S and stores the MAP values for an instance I on a DP table storage
 // at the position designated by the ticket tk. Returns S and the ticket used (if tk < 0,
 // StoreMAP creates a new ticket).
-func StoreMAP(S SPN, I VarSet, tk int, storage *Storer) (SPN, int) {
+func StoreMAP(S SPN, I VarSet, tk int, storage *Storer) (SPN, int, VarSet) {
 	if tk < 0 {
 		tk = storage.NewTicket()
 	}
 
-	visited := make(map[SPN]bool)
-	c, _c := &common.Stack{}, &common.Queue{}
-	c.Give(S)
-	visited[S] = true
-	_c.Give(S)
+	Q, T := common.Queue{}, common.Stack{}
+	V := make(map[SPN]bool)
+	tab, _ := storage.Table(tk)
 
-	for !_c.Empty() {
-		s := _c.Take().(SPN)
+	Q.Enqueue(S)
+	T.Push(S)
+	V[S] = true
+
+	for !Q.Empty() {
+		s := Q.Dequeue().(SPN)
 		ch := s.Ch()
-		for _, cs := range ch {
-			if !visited[cs] {
-				_c.Give(cs)
-				c.Give(cs)
-				visited[cs] = true
+		for _, c := range ch {
+			if !V[c] {
+				Q.Enqueue(c)
+				T.Push(c)
+				V[c] = true
 			}
 		}
 	}
 
-	_c, visited = nil, nil // free memory as soon as soon as the garbage collector allows
-	sys.Free()
-	table, _ := storage.Table(tk)
-	for !c.Empty() {
-		s := c.Take().(SPN)
+	for !T.Empty() {
+		s := T.Pop().(SPN)
 		switch t := s.Type(); t {
 		case "leaf":
-			table.StoreSingle(s, s.Value(I))
+			m := s.Max(I)
+			tab.StoreSingle(s, m)
 		case "sum":
 			sum := s.(*Sum)
-			ch := sum.Ch()
 			W := sum.Weights()
-			n := len(ch)
-			vals := make([]float64, n)
-			for i, cs := range ch {
-				v, e := table.Single(cs)
-				if !e {
-					// Should never occur. Just in case what I thought of is flawed.
-					fmt.Println("Something terrible has just happened. (StoreInference:learn/derive.go)")
+			ch := s.Ch()
+			mv := math.Inf(-1)
+			for i, c := range ch {
+				v, _ := tab.Single(c)
+				u := math.Log(W[i]) + v
+				if u > mv {
+					mv = u
 				}
-				vals[i] = v + math.Log(W[i])
 			}
-			table.StoreSingle(s, sum.Compute(vals))
+			tab.StoreSingle(s, mv)
 		case "product":
-			prod := s.(*Product)
-			ch := prod.Ch()
-			n := len(ch)
-			vals := make([]float64, n)
-			for i, cs := range ch {
-				vals[i], _ = table.Single(cs)
+			ch := s.Ch()
+			var v float64
+			for _, c := range ch {
+				cv, _ := tab.Single(c)
+				v += cv
 			}
-			table.StoreSingle(s, prod.Compute(vals))
+			tab.StoreSingle(s, v)
 		}
 	}
-	c = nil
-	sys.Free()
-	return S, tk
+
+	V = make(map[SPN]bool)
+	Q.Enqueue(S)
+	V[S] = true
+	M := make(VarSet)
+
+	for !Q.Empty() {
+		s := Q.Dequeue().(SPN)
+		switch t := s.Type(); t {
+		case "leaf":
+			N, _ := s.ArgMax(I)
+			for k, v := range N {
+				M[k] = v
+			}
+		case "sum":
+			sum := s.(*Sum)
+			W := sum.Weights()
+			ch := s.Ch()
+			m := math.Inf(-1)
+			var mvc SPN
+			for i, c := range ch {
+				v, _ := tab.Single(c)
+				u := math.Log(W[i]) + v
+				if u > m && !V[c] {
+					m, mvc = u, c
+				}
+			}
+			Q.Enqueue(mvc)
+		case "product":
+			ch := s.Ch()
+			for _, c := range ch {
+				if !V[c] {
+					Q.Enqueue(c)
+				}
+			}
+		}
+		V[s] = true
+	}
+
+	return S, tk, M
 }
---
+++ spn/utils_test.go
@@ -0,0 +1,102 @@
+package spn
+
+import (
+	"github.com/RenatoGeh/gospn/common"
+	"reflect"
+	"testing"
+)
+
+func sampleSPN() SPN {
+	R := NewSum()
+	P1, P2 := NewProduct(), NewProduct()
+	f1, f2 := NewMultinomial(0, []float64{0.9, 0.1}), NewMultinomial(1, []float64{0.6, 0.4})
+	S1, S2, S3, S4 := NewSum(), NewSum(), NewSum(), NewSum()
+	Y11, Y12 := NewMultinomial(2, []float64{0.8, 0.2}), NewMultinomial(2, []float64{0.3, 0.7})
+	Y21, Y22 := NewMultinomial(3, []float64{0.4, 0.6}), NewMultinomial(3, []float64{0.9, 0.1})
+
+	R.AddChildW(P1, 0.3)
+	R.AddChildW(P2, 0.7)
+	P1.AddChild(f1)
+	P1.AddChild(S1)
+	P1.AddChild(S3)
+	P2.AddChild(S2)
+	P2.AddChild(S4)
+	P2.AddChild(f2)
+	S1.AddChildW(Y11, 0.6)
+	S1.AddChildW(Y12, 0.4)
+	S2.AddChildW(Y11, 0.2)
+	S2.AddChildW(Y12, 0.8)
+	S3.AddChildW(Y21, 0.9)
+	S3.AddChildW(Y22, 0.1)
+	S4.AddChildW(Y21, 0.5)
+	S4.AddChildW(Y22, 0.5)
+
+	return R
+}
+
+func doBFS(S SPN, f func(SPN) bool) {
+	c := common.Queue{}
+	visited := make(map[SPN]bool)
+	c.Enqueue(S)
+	visited[S] = true
+
+	for !c.Empty() {
+		s := c.Dequeue().(SPN)
+		if !f(s) {
+			break
+		}
+		ch := s.Ch()
+		for _, cs := range ch {
+			if !visited[cs] {
+				c.Enqueue(cs)
+				visited[cs] = true
+			}
+		}
+	}
+	visited = nil
+}
+
+// TestStoreInference tests StoreInference.
+func TestStoreInference(t *testing.T) {
+	S := sampleSPN()
+	I := make(VarSet)
+	I[0], I[2] = 1, 0
+	st := NewStorer()
+	_, tk := StoreInference(S, I, -1, st)
+	doBFS(S, func(s SPN) bool {
+		v := s.Value(I)
+		u, _ := st.Single(tk, s)
+		if v != u {
+			t.Errorf("Expected (Value) %f == %f (StoreInference) ? true, got false.", v, u)
+			return false
+		}
+		return true
+	})
+}
+
+// TestStoreMap tests StoreMAP.
+func TestStoreMAP(t *testing.T) {
+	S := sampleSPN()
+	I := make(VarSet)
+	I[0], I[2] = 1, 0
+	st := NewStorer()
+	_, tk, M := StoreMAP(S, I, -1, st)
+	//doBFS(S, func(s SPN) bool {
+	//v := s.Max(I)
+	//u, _ := st.Single(tk, s)
+	//if v != u {
+	//t.Errorf("Expected (Max) %f == %f (StoreMAP) ? true, got false.", v, u)
+	//return false
+	//}
+	//return true
+	//})
+	//N, _ := S.ArgMax(I)
+	N, v := S.ArgMax(I)
+	u, _ := st.Single(tk, S)
+	if v != u {
+		t.Errorf("Expected (Max) %f == %f (StoreMAP) ? true, got false.", v, u)
+	}
+	if !reflect.DeepEqual(M, N) {
+		t.Errorf("Expected MAP states to be equal, got different.\n  M=%v\n  N=%v", M, N)
+	}
+}
